ec0a097b2f0395636a685f592c648ac20aeb1331
bicoupler <ireallylovemakingmoney@gmail.com>
2024-09-27 17:21:19 +0100

    Coloured Bricks and Cylindrical Tower

--- games-engines-2024/scenes/wall.gd
+++ games-engines-2024/scenes/wall.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var brick_scene: PackedScene
 
 @export var rows = 10
-@export var cols = 10
+@export var cols = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +11,7 @@ func _ready() -> void:
 	for row in range(rows):
 		for col in range(cols):
 			var brick = brick_scene.instantiate()
-			var pos = Vector3(col, row, 0)
+			var pos = Vector3(cos(col) * PI , row, sin(col) * PI)
 			brick.position = pos
 			add_child(brick)
 			
