9535988b5e3633228ecf8177391dd7c549b48ce5
bicoupler <ireallylovemakingmoney@gmail.com>
2024-10-04 15:57:34 +0100

    Week 3 2?

--- games-engines-2024/scenes/player.gd
+++ games-engines-2024/scenes/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
-@export var speed: float = -1
-@export var rot_speed = 1.0
+@export var speed: float = 10
+@export var rot_speed = 180.0
 
+@export var bullet_scene: PackedScene
+@export var bullet_target: Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,7 +12,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	#position.z += speed * delta
 	
 	#rotate_y(deg_to_rad(rot_speed) * delta)
@@ -19,10 +21,38 @@ func _process(delta: float) -> void:
 	
 	#global_position.z += speed * delta
 	
-	var f = Input.get_axis("move_back","move_forward")
-	var r = Input.get_axis("move_left", "move_right")
+	var f = Input.get_axis("move_forward","move_back")
+	var r = Input.get_axis("move_right", "move_left")
 	
-	translate(Vector3(0,0,f * delta * speed))
-	rotate_y(r * rot_speed * delta)
+	var v = Vector3()
+	
+	v = global_transform.basis.z
+	
+	print(v)
+	
+	velocity = v * speed * f
+	
+	move_and_slide()
+	#translate(Vector3(0,0,f * delta * speed))
+	rotate_y(deg_to_rad(rot_speed) * r * delta)
+	
+	DebugDraw2D.set_text("position", position)
+	DebugDraw2D.set_text("global_position", global_position)
+	DebugDraw2D.set_text("basis.x", transform.basis.x)
+	DebugDraw2D.set_text("basis.y", transform.basis.y)
+	DebugDraw2D.set_text("basis.z", transform.basis.z)
+	
+	DebugDraw2D.set_text("glo basis.x", global_transform.basis.x)
+	DebugDraw2D.set_text("glo basis.y", global_transform.basis.y)
+	DebugDraw2D.set_text("glo basis.z", global_transform.basis.z)
+
+	if Input.is_action_just_pressed("Shoot"):
+		var bullet = bullet_scene.instantiate()
+		
+		get_parent().add_child(bullet)
+
+		bullet.global_position = bullet_target.global_position
+		bullet.global_rotation = global_rotation
+		
 	
 	pass
--- games-engines-2024/scenes/wall.gd
+++ games-engines-2024/scenes/wall.gd
@@ -1,20 +1,27 @@
 extends Node3D
 
-@export var brick_scene: PackedScene
-
+@export var brick_scene:PackedScene
 @export var rows = 10
-@export var cols = 1000
+@export var cols = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
 	for row in range(rows):
 		for col in range(cols):
 			var brick = brick_scene.instantiate()
-			var pos = Vector3(cos(col) * PI , row, sin(col) * PI)
+			var pos = Vector3(col, row, 0)
 			brick.position = pos
-			add_child(brick)
 			
+			var m = StandardMaterial3D.new()
+			var h = ((row * col) + col) / (float)(rows * col)
+			
+			m.albedo_color = Color.from_hsv(h, 1, 1)
+			
+			var mesh: MeshInstance3D = brick.get_node("MeshInstance3D")
+			mesh.set_surface_override_material(0, m)
+			
+			add_child(brick)
+	
 	pass # Replace with function body.
 
 
